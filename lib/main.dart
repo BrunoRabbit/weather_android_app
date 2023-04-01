990b114a19978ef1321a3a5e8e1e334404db109e
Bruno Coelho <brunoca2000@hotmail.com>
2023-04-01 18:59:22 -0300

    feat: init Hive and add new fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:weather_android_app/modules/register/data/digest_adapter.dart';
 import 'package:weather_android_app/routes/app_routes.dart';
 
-void main() => runApp(const MyApp());
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+  await Hive.initFlutter();
+  await dotenv.load(fileName: '.env');
 
-  @override
-  State<MyApp> createState() => _MyAppState();
+  Hive.registerAdapter(DigestAdapter());
+
+  runApp(const WeatherApp());
 }
 
-class _MyAppState extends State<MyApp> {
+class WeatherApp extends StatefulWidget {
+  const WeatherApp({Key? key}) : super(key: key);
+
+  @override
+  State<WeatherApp> createState() => _WeatherAppState();
+}
+
+class _WeatherAppState extends State<WeatherApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       onGenerateRoute: AppRouter.generateRoute,
+      theme: ThemeData(
+        fontFamily: 'Nunito-Light',
+      ),
     );
   }
 }
