cdb835622be6372a74bfa51127ddce24616cfca2
Bruno Coelho <brunoca2000@hotmail.com>
2023-04-03 16:19:15 -0300

    chore: add images and rename variables in app utility

--- lib/utility/app_utility.dart
+++ lib/utility/app_utility.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppUtility {
-  static const Color primaryColor = Color(0xFF1157BC);
-  static const Color secondaryColor = Color(0xFF2E9DE2);
+  static const Color kPrimaryColor = Color(0xFF1157BC);
+  static const Color kSecondaryColor = Color(0xFF2E9DE2);
 
-  static const Color primaryBackground = Color(0xff101E41);
-  static const Color secondaryBackground = Color(0xff1E78CE);
+  static const Color kPrimaryBackground = Color(0xff101E41);
+  static const Color kSecondaryBackground = Color(0xff1E78CE);
 
-  static const Color formBackground =  Color(0xff1F2E4B);
+  static const Color kFormBackground =  Color(0xff1F2E4B);
 
+  static const Color kAccentColor = Color(0xff6100E9);
 }
