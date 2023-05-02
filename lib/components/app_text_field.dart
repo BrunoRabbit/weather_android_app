654cc9f967cb0ab3b0cc7db37f3d88f252ebdc47
Bruno Coelho <brunoca2000@hotmail.com>
2023-05-02 16:10:14 -0300

    feat: create text field component

--- lib/components/app_text_field.dart
+++ lib/components/app_text_field.dart
@@ -1,55 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:weather_android_app/utility/app_utility.dart';
-import 'package:weather_android_app/utility/text_utility.dart';
+import 'package:flutter/services.dart';
+import 'package:weather_android_app/components/app_text.dart';
+import 'package:weather_android_app/utils/utility/app_utility.dart';
+import 'package:weather_android_app/utils/utility/text_utility.dart';
 
 class AppTextField extends StatelessWidget {
   final String? Function(String?)? validator;
   final String labelText;
   final TextEditingController controller;
+  final List<TextInputFormatter>? textInputFormatter;
+  final bool obscureText;
+  final void Function(String)? onChanged;
 
   const AppTextField({
     Key? key,
     this.validator,
     required this.labelText,
     required this.controller,
+    this.textInputFormatter,
+    this.obscureText = false,
+    this.onChanged,
   }) : super(key: key);
-
-  final Color colorTxt = Colors.white;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 40),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          AppText(
             labelText,
-            style: TextUtility.body2.bold.copyWith(color: colorTxt),
+            family: 'SemiBold',
           ),
           const SizedBox(
-            height: 6,
+            height: 8,
           ),
           TextFormField(
             validator: validator,
             controller: controller,
+            inputFormatters: textInputFormatter,
+            obscureText: obscureText,
+            onChanged: onChanged,
+            cursorColor: AppUtility.kSecondaryBackground,
+            style: const TextStyle(color: Colors.white),
             decoration: InputDecoration(
               filled: true,
-              fillColor: AppUtility.formBackground,
+              fillColor: AppUtility.kFormBackground,
               contentPadding: const EdgeInsets.symmetric(horizontal: 10),
               floatingLabelBehavior: FloatingLabelBehavior.always,
               hintText: 'Digite aqui...',
               hintStyle: TextUtility.body2.copyWith(
-                color: colorTxt.withOpacity(.4),
+                color: Colors.white.withOpacity(.4),
+                fontFamily: 'Nunito-Medium',
               ),
-              errorStyle: TextUtility.error1,
+              errorStyle: TextUtility.error1.copyWith(
+                fontFamily: 'Nunito-Regular',
+              ),
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(8.0),
                 borderSide: BorderSide.none,
               ),
               errorBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(8.0),
-                borderSide: BorderSide.none,
+                borderSide: BorderSide(
+                  color: AppUtility.kErrorColor.withOpacity(.6),
+                ),
               ),
             ),
           ),
