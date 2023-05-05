8c2dec339c6c80f3a20fa2988399868d37d95707
Bruno Coelho <brunoca2000@hotmail.com>
2023-05-05 18:25:24 -0300

    chore: minor changes

--- test/modules/visibility/visibility_view_model_test.dart
+++ test/modules/visibility/visibility_view_model_test.dart
@@ -1,7 +1,4 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobx/mobx.dart';
 import 'package:weather_android_app/modules/visibility/entity/weather_entity.dart';
